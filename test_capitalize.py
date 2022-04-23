47274b4522b9a751352220e16bcd712d5de46378
Mrinal Gosain <mrizzlybear@Mrinals-MacBook-Air.local>
2022-04-23 14:53:06 -0700

    added docstring

--- test_capitalize.py
+++ test_capitalize.py
@@ -1,9 +1,11 @@
 # test_capitalize.py
 
 def capitalize_string(s):
+    """ test 1"""
     if not isinstance(s, str):
         raise TypeError('Please provide a string')
     return s.capitalize()
 
 def test_capitalize_string():
+    """ test 2"""
     assert capitalize_string('test') == 'Test'
